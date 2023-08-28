2970d68b13e338bf3cbdf50a6658919d52ff153d
Melissa <melissaguillen70@yahoo.com>
2023-08-27 21:46:13 -0700

    changes to URL

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -15,7 +15,7 @@ export default function ContactList({ setSelectedContactId }) {
   useEffect(() => {
       async function fetchContacts() {
           try {
-              const response = await fetch(`http://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`
+              const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`
               );
               const result = await response.json();
               setContacts(result);
